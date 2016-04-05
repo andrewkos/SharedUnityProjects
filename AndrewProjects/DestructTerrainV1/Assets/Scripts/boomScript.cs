1cd8d1e98f15ee23c480ca4d1b3061d2cb75403a
andrewkos <andrew_kos@hotmail.com>
2016-04-05 23:51:42 +0100

    Fixed lack of destruction

--- AndrewProjects/DestructTerrainV1/Assets/Scripts/boomScript.cs
+++ AndrewProjects/DestructTerrainV1/Assets/Scripts/boomScript.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class boomScript : MonoBehaviour {
 
     public System.DateTime killTime;
+    private SphereCollider sphere;
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("BOOM!");
-        killTime = System.DateTime.Now.AddMilliseconds(4000);
+        killTime = System.DateTime.Now.AddMilliseconds(1000);
+        sphere = GetComponent<SphereCollider>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 	    if (System.DateTime.Now > killTime) { Destroy(gameObject); }
         gameObject.transform.localScale *= 1.01f;
-	}
+        sphere.radius *= 1.005f;
 
-    void OnCollisionExit(Collision col)
+    }
+
+    void OnCollisionEnter(Collision col)
     {
         Debug.Log("BOOM-HIT");
         if (col.gameObject.tag == "Replacer")
