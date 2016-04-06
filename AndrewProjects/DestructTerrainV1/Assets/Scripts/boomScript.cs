2e302aa7226270a353b4a261ba175b2bb9f9d8aa
andrewkos <andrew_kos@hotmail.com>
2016-04-06 23:32:56 +0100

    Update1

--- AndrewProjects/DestructTerrainV1/Assets/Scripts/boomScript.cs
+++ AndrewProjects/DestructTerrainV1/Assets/Scripts/boomScript.cs
@@ -4,24 +4,20 @@ using System.Collections;
 public class boomScript : MonoBehaviour {
 
     public System.DateTime killTime;
-    private SphereCollider sphere;
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("BOOM!");
-        killTime = System.DateTime.Now.AddMilliseconds(1000);
-        sphere = GetComponent<SphereCollider>();
+        killTime = System.DateTime.Now.AddMilliseconds(4000);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 	    if (System.DateTime.Now > killTime) { Destroy(gameObject); }
         gameObject.transform.localScale *= 1.01f;
-        sphere.radius *= 1.005f;
+	}
 
-    }
-
-    void OnCollisionEnter(Collision col)
+    void OnCollisionExit(Collision col)
     {
         Debug.Log("BOOM-HIT");
         if (col.gameObject.tag == "Replacer")
